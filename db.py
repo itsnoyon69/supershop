03cb426532326e7cb62c098a04db7280c4492b27
Islam <naimul.islam@savethechildren.org>
2026-05-13 12:10:39 +0600

    connected final_data table

--- db.py
+++ db.py
@@ -1,6 +1,11 @@
 import psycopg2
 
 def get_conn():
+    query = "SELECT * FROM merge"
+
+    df = pd.read_sql(query, conn)
+
+    return df
 
     conn = psycopg2.connect(
         host="aws-1-ap-northeast-1.pooler.supabase.com",
