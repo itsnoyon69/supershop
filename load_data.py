2e4933fa7266cd4035fe3600f1a4e24ea7d41713
Islam <naimul.islam@savethechildren.org>
2026-05-13 12:14:07 +0600

    connected final_data table

--- load_data.py
+++ load_data.py
@@ -1,32 +1,16 @@
+```python id="k4m8x2"
 import pandas as pd
 from db import get_conn
 
 def load_data():
+
     conn = get_conn()
 
-    customers = pd.read_sql("SELECT * FROM customers", conn)
-    products = pd.read_sql("SELECT * FROM products", conn)
-    orders = pd.read_sql("SELECT * FROM orders", conn)
-    sales = pd.read_sql("SELECT * FROM sales", conn)
+    query = "SELECT * FROM merge"
 
-    df = sales.merge(orders, on='order_id', how='left') \
-              .merge(customers, on='customer_id', how='left') \
-              .merge(products, on='product_id', how='left', suffixes=('', '_drop'))
-
-    df = df[[c for c in df.columns if not c.endswith('_drop')]]
-
-    df.to_csv(
-        "merged_dataset.csv",
-        index=False
-    )
-
-    print(df.head())
-
-    if 'quantity_y' in df.columns:
-        df['quantity'] = df['quantity_y']
-    elif 'quantity_x' in df.columns:
-        df['quantity'] = df['quantity_x']
+    df = pd.read_sql(query, conn)
 
     df['order_date'] = pd.to_datetime(df['order_date'])
 
     return df
+```
