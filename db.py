d71cc08d968b849effad9139c8dbb3917296c650
Islam <naimul.islam@savethechildren.org>
2026-05-13 12:03:39 +0600

    supabase connection fixed

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 import psycopg2
 
 def get_conn():
-    return psycopg2.connect(
-        dbname="business analysis",
-        user="postgres",
-        password="noyon66966",
-        host="localhost",
+
+    conn = psycopg2.connect(
+        host="aws-1-ap-northeast-1.pooler.supabase.com",
+        database="postgres",
+        user="postgres.lrfshbpmjqevlookcrmx",
+        password="#itsnoyon69#",
         port="5432"
     )
+
+    return conn
